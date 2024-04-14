abe46f78792b7d51f764168afe3be6c8f6ae26d8
YannineSaavedra <162144036+YannineSaavedra@users.noreply.github.com>
2024-04-14 10:11:11 -0400

    Prueba 18

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -70,8 +70,16 @@ void exercise_9(string s) {
 }
 
 int exercise_10(int a, int b) {
-  // TODO: YOUR CODE HERE
-  return 0;
+  int a,b,mcm=1;
+	cin>>a>>b;
+	for(int n=1;n<=a;n++)
+	{
+		if((a%n==0)and(b%n==0))
+		{
+			mcm=n;
+		}
+	}	
+	return mcm;
 }
 
 void exercise_11() {
