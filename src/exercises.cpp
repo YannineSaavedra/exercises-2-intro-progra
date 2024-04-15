78c03e88950bdab15aade1d1f156e7f1a61c7359
YannineSaavedra <162144036+YannineSaavedra@users.noreply.github.com>
2024-04-14 23:12:57 -0400

    Prueba 47

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -119,8 +119,33 @@ void exercise_7(int n) {
 }
 
 void exercise_8(string s) {
-  // TODO: YOUR CODE HERE
+    int s_size=s.size();
+    string s_2;
+    for(int i=0;s_size-1>=i ; ++i){
+
+        char s_letter = s[i];
+        if(s_letter!=' '){
+            s_2 = s_2 + s_letter;
+        }
+    }
+    bool esPalindromo = true;
+    int n = s_2.size();
+
+    for (int i = 0; i < n / 2; ++i) {
+        if (s_2[i] != s_2[n - 1 - i]) {
+            esPalindromo = false;
+            break;
+        }
+    }
+
+    if(esPalindromo) {
+        cout << "YES" << endl;
+    }else {
+        cout << "NO" << endl;
+    }
+
 }
+
 
 void exercise_9(string s) {
   // TODO: YOUR CODE HERE
